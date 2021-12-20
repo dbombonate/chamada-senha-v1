6e9c35e9e53f014b440c4e42fa39b67d85371b6b
Daniel Bombonate | DEP Systems <bombonate.ti@gmail.com>
2021-12-20 17:26:47 -0300

    Database config and initialization

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const routes = require('./routes');
+const database = require('./database');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
--- src/database/index.js
+++ src/database/index.js
@@ -1,8 +1,17 @@
+require('dotenv').config({
+  path: process.env.NODE_ENV === "test" ? ".env.test" : ".env"
+});
+
 const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize({
   dialect: 'sqlite',
-  storage: 'src/database/db/senhas.db'
+  storage: process.env.STORAGE,
+  logging: false,
 });
+
+sequelize.authenticate()
+.then(() => { console.log('Database Ready')})
+.catch((error) => { console.error('Database Error', error)});
 
 module.exports = sequelize;
