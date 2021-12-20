85810bd1294efab8bee244872e67e24ad0d1890b
Daniel Bombonate | DEP Systems <bombonate.ti@gmail.com>
2021-12-20 18:00:20 -0300

    Add sync database

--- src/app.js
+++ src/app.js
@@ -3,6 +3,9 @@ const app = express();
 const routes = require('./routes');
 const database = require('./database');
 
+database.sync()
+.then((value) => console.log(value.options));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
