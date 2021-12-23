f436346dc0c25546a57f885103462d2385b3677c
Daniel Bombonate | DEP Systems <bombonate.ti@gmail.com>
2021-12-23 17:38:09 -0300

    Add routes and controller

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const routes = express.Router();
 
+const senhaController = require('./app/controllers/senhasController');
+
 routes.get('/', (req,res) => {
   res.send({ message: "Api de Chamada de Senhas"})
 });
 
+routes.post('/novaSenha', senhaController.novaSenha );
+routes.post('/', senhaController.chamaSenhaDigitada);
+
+routes.get('/proximaSenha', senhaController.chamaNovaSenha);
 module.exports = routes;
