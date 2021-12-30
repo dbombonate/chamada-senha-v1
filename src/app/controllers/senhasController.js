120e7d3bc8f7b28a25230e64124fa02cf6d99b41
Daniel Bombonate | DEP Systems <bombonate.ti@gmail.com>
2021-12-30 18:15:49 -0300

    Add validation for previous pass not called

--- src/app/controllers/senhasController.js
+++ src/app/controllers/senhasController.js
@@ -1,3 +1,4 @@
+const { Op } = require('sequelize');
 const Senha = require('../models/Senha');
 
 class SenhaController {
@@ -18,7 +19,20 @@ class SenhaController {
       const id = req.body.id;
       const data = await Senha.findByPk(id);
       if(data === null) return res.status(200).send({ message: 'ID do not exists.'});
-      return res.status(200).send({ id: data.dataValues.id});
+      //Busca por senha prioridade pendente
+      const senhaPrioridadePendente = await Senha.findAll({ where: { 
+        senha_status: false,
+        senha_priority: true,
+      }});
+      //Busca por senha normal pendente
+      const senhaNormalPendente = await Senha.findAll({ where: { 
+        senha_status: false,
+        id: {
+          [Op.lt]: id
+        }
+      }});
+      if(senhaNormalPendente || senhaPrioridadePendente) return res.status(403).send({ message: 'There are other pass to call before'});
+      return res.status(200).send({ senha: data.dataValues.id});
     } catch (error) {
       console.log(error.message);
       return res.status(400).send({ error: error.message });
@@ -30,7 +44,7 @@ class SenhaController {
       //Retornar a próxima senha a ser chamada
       const filaSenhas = await Senha.findAll({ where: { senha_status: false}});
       let proximaSenha = filaSenhas[0];
-      return res.status(200).send({ id: proximaSenha.dataValues.id });
+      return res.status(200).send({ senha: proximaSenha.dataValues.id });
     } catch (error) {
       console.log(error.message);
       return res.status(400).send({ error: error.message });
