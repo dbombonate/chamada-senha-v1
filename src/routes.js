434b83bfb44880eea031585953f5c7a38e174f22
Daniel Bombonate | DEP Systems <bombonate.ti@gmail.com>
2022-01-19 08:11:22 -0300

    Alter on routes and change controller

--- src/routes.js
+++ src/routes.js
@@ -2,13 +2,15 @@ const express = require('express');
 const routes = express.Router();
 
 const senhaController = require('./app/controllers/senhasController');
+const newPasswordController = require('./app/controllers/NewPasswordController');
 
 routes.get('/', (req,res) => {
-  res.send({ message: "Api de Chamada de Senhas"})
+  res.send({ message: "Password Call API"})
 });
 
-routes.post('/novaSenha', senhaController.novaSenha );
-routes.post('/', senhaController.chamaSenhaDigitada);
+routes.post('/api/passwords', newPasswordController.handle);
 
-routes.get('/proximaSenha', senhaController.chamaNovaSenha);
+routes.get('/api/:password', senhaController.chamaSenhaDigitada);
+routes.get('/api/next_password', senhaController.chamaNovaSenha);
+
 module.exports = routes;
