ee21c6c44a148490a9b3ea62ed21fb2cf95aa432
Daniel Bombonate | DEP Systems <bombonate.ti@gmail.com>
2022-01-19 17:24:10 -0300

    Add Error message if password is false

--- src/app/services/NewPasswordsService.js
+++ src/app/services/NewPasswordsService.js
@@ -5,6 +5,10 @@ class NewPasswordsService {
   async execute({ senha_priority }){
 
     const password = await Senha.create({ senha_priority });
+
+    if(!password){
+      throw new Error('Something went wrong during saving on Database');
+    }
     
     return password.id;
 
