99c164860baf186e517fc244fa91d7b13003f95c
Daniel Bombonate | DEP Systems <bombonate.ti@gmail.com>
2022-02-22 22:37:04 -0300

    Add CORS configuration middleware

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 require('express-async-errors');
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const routes = require('./routes');
 const database = require('./database');
@@ -11,6 +12,14 @@ const database = require('./database');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use((req, res, next) => {
+	//Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "*" indicando que qualquer site pode fazer a conexão
+    res.header("Access-Control-Allow-Origin", "*");
+	//Quais são os métodos que a conexão pode realizar na API
+    res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
+    app.use(cors());
+    next();
+});
 
 app.use(routes);
 
