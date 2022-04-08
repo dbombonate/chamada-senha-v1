fff06f99da4f2944d9fbd567ea92bbe341c8a9f0
Daniel Bombonate | DEP Systems <bombonate.ti@gmail.com>
2022-04-07 21:43:00 -0300

    Model refactoring

--- src/app/models/Store.js
+++ src/app/models/Store.js
@@ -1,22 +1,31 @@
 const { Sequelize, DataTypes, Model } = require('sequelize');
-const sequelize = require('../../database');
 
-class Store extends Model {};
-
-Store.init({
-  id: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    autoIncrement: true,
-    primaryKey: true,
-  },
-  name: {
-    type: DataTypes.STRING,
-    allowNull: false,
+class Store extends Model {
+  static init(sequelize) {
+    super.init({
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      store_name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        defaultValue: '',
+        validate:
+        {
+          len: {
+            args: [2, 80],
+            msg: 'Store name must have between 2 to 80 characters.'
+          }
+        }
+      }
+    }, {
+      sequelize,
+    });
+    return this;
   }
-}, {
-  sequelize,
-  modelName: 'Department',
-});
+};
 
 module.exports = Store;
