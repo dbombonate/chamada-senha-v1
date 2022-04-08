9fd7545b12bdd1153a39718adea51b4cfbc58503
Daniel Bombonate | DEP Systems <bombonate.ti@gmail.com>
2022-04-07 21:43:10 -0300

    Model refactoring

--- src/app/models/Department.js
+++ src/app/models/Department.js
@@ -1,27 +1,36 @@
 const { Sequelize, DataTypes, Model } = require('sequelize');
 const sequelize = require('../../database');
 
-class Department extends Model {};
-
-Department.init({
-  id: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    autoIncrement: true,
-    primaryKey: true,
-  },
-  name: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  dept_status: {
-    type: DataTypes.BOOLEAN,
-    allowNull: false,
-    defaultValue: true,  
+class Department extends Model {
+  static init(sequelize) {
+    super.init({
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      dept_name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        defaultValue: '',
+        validate: {
+          len: {
+            args: [2, 80],
+            msg: 'Department name must have 2 to 80 characters.'
+          }
+        }
+      },
+      dept_status: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        defaultValue: true,
+      },
+    }, {
+      sequelize,
+    });
+    return this;
   }
-}, {
-  sequelize,
-  modelName: 'Department',
-});
+}
 
 module.exports = Department;
